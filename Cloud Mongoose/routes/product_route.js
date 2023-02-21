2239d0e064bc02224834675ae74d1a8c3c9c387d
Sajithmym <Sajithmym@gmail.com>
2023-02-21 23:37:33 +0530

    all

--- Cloud Mongoose/routes/product_route.js
+++ Cloud Mongoose/routes/product_route.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { findByIdAndRemove } = require("../model/Product")
 const Product = require("../model/Product")
 const router = express.Router()
 
@@ -26,6 +27,40 @@ router.post("/", async (req, res, next) => {
         next()
     }
     res.status(201).json({ product })
+})
+
+router.get("/:id", async (req, res, next) => {
+    let Id = req.params.id
+    let product = await Product.findById(Id)
+    if (!product)
+        return res.status(404).json({ message: "No Product Found" })
+    res.status(200).json({ product }) 
+
+})
+
+router.patch("/:id", async (req, res, next) => {
+    let Id = req.params.id
+    let product = await Product.findByIdAndUpdate(Id, {
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        Image_Link: req.body.Image_Link,
+        Quantity: req.body.Quantity,
+        isFeatured: req.body.isFeatured
+    })
+    product = await product.save()
+    if (!product)
+        return res.status(500).json({ message: "Can't Save Product..." })
+    res.status(200).json({ product })
+
+})
+
+router.delete("/:id", async (req, res, next) => {
+    let Id = req.params.id
+    let product = await Product.findByIdAndRemove(Id)
+    if (!product)
+        return res.status(500).json({ message: "Can't Delete..." })
+    res.status(200).json({ message: "Deleted Successfully..." })
 })
 
 module.exports = router
